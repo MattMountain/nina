8c63958a2f752c6c764e31d89b884b50fa6e5987
MattMountain <digital@matthiasriedl.com>
2021-01-14 18:51:41 +0100

    add social fixes

--- components/socialMedia.js
+++ components/socialMedia.js
@@ -5,19 +5,27 @@ export default function SocialMedia(data)
     const social = data.data
     let fileUrls = []
 
-    social.forEach(item => fileUrls.push(`/svg/${ item.node.label.toLowerCase() }.svg` ))
+    social?.forEach(item => fileUrls.push(`/svg/${ item.node.label.toLowerCase() }.svg` ))
+    if (social)
+    {
+        return (
+            <section className='social-media'>
+                { Object.keys(social).length > 0 ? (
+                        Object.keys(social).map( (key) =>
+                            <a key={ key } href={ social[key].node.url } target="_blank" className='social-media-link'>
+                                <Image src={ fileUrls[key] } alt={ social[key].node.label } width="30" height="30" />
+                            </a>) )
+                    : (<span></span>)
+                }
 
-    return (
-        <section className='social-media'>
-            { Object.keys(social).length > 0 ? (
-                Object.keys(social).map( (key) =>
-                    <a href={ social[key].node.url } target="_blank" className='social-media-link'>
-                        <Image src={ fileUrls[key] } alt={ social[key].node.label } width="30" height="30" />
-                    </a>) )
-                : (<span></span>)
-            }
+            </section>
 
-        </section>
+        )
+    } else
+    {
+        return (
+            <span></span>
+        )
+    }
 
-    )
 }
