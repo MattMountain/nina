2f58d25e27e4c340cebde90b643eff0be3f9771f
MattMountain <digital@matthiasriedl.com>
2021-01-19 15:51:38 +0100

    refactor thumbnails

--- functions/thumbnails.js
+++ functions/thumbnails.js
@@ -5,9 +5,9 @@ export async function filterThumbnails(categories)
     const children = categories?.node?.children?.edges;
     let thumbnails = new Array();
 
-    children.forEach(async (item) => {
-        const name = item.node.name ;
-        thumbnails.push({ name: name, thumbnail: await getThumbnails(item.node.name) })
+    children?.forEach(async (item) => {
+        const name = item?.node?.name ;
+        thumbnails.push({ name: name, thumbnail: await getThumbnails(item?.node?.name) })
     })
     return thumbnails
 }
